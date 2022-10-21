1ef3f775b9878de518ccf903a65c692fa5ffaecb
trey368s <84042739+trey368s@users.noreply.github.com>
2022-10-21 14:57:36 -0400

    .NET Framework 4.8 Implementation

---
+++ IT Returns Label Maker/IT Returns Label Maker/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace IT_Returns_Label_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.h1label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabelKb = new System.Windows.Forms.LinkLabel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.locationLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipLabel = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
+            this.textBoxZip = new System.Windows.Forms.TextBox();
+            this.labelError = new System.Windows.Forms.Label();
+            this.linkLabelURL = new System.Windows.Forms.LinkLabel();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // h1label
+            // 
+            this.h1label.AutoSize = true;
+            this.h1label.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.h1label.Location = new System.Drawing.Point(12, 9);
+            this.h1label.Name = "h1label";
+            this.h1label.Size = new System.Drawing.Size(252, 19);
+            this.h1label.TabIndex = 0;
+            this.h1label.Text = "Senneca Holdings IT Returns";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(469, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please only return laptops, cell phones, or other equipment listed";
+            // 
+            // linkLabelKb
+            // 
+            this.linkLabelKb.AutoSize = true;
+            this.linkLabelKb.Location = new System.Drawing.Point(481, 38);
+            this.linkLabelKb.Name = "linkLabelKb";
+            this.linkLabelKb.Size = new System.Drawing.Size(42, 14);
+            this.linkLabelKb.TabIndex = 2;
+            this.linkLabelKb.TabStop = true;
+            this.linkLabelKb.Text = "here.";
+            this.linkLabelKb.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelKb_LinkClicked);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(13, 74);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(42, 14);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name:";
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Location = new System.Drawing.Point(13, 116);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(154, 14);
+            this.weightLabel.TabIndex = 4;
+            this.weightLabel.Text = "Overestimated Weight:";
+            // 
+            // locationLabel
+            // 
+            this.locationLabel.AutoSize = true;
+            this.locationLabel.Location = new System.Drawing.Point(13, 162);
+            this.locationLabel.Name = "locationLabel";
+            this.locationLabel.Size = new System.Drawing.Size(70, 14);
+            this.locationLabel.TabIndex = 5;
+            this.locationLabel.Text = "Location:";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(13, 208);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(98, 14);
+            this.phoneLabel.TabIndex = 6;
+            this.phoneLabel.Text = "Phone Number:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(61, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(203, 22);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(173, 113);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(91, 22);
+            this.textBoxWeight.TabIndex = 8;
+            // 
+            // comboBoxLocation
+            // 
+            this.comboBoxLocation.FormattingEnabled = true;
+            this.comboBoxLocation.Items.AddRange(new object[] {
+            "Cincinnati",
+            "Irving",
+            "Kalamazoo",
+            "Mankato",
+            "Redmond",
+            "Salt Lake City",
+            "Other Return Address"});
+            this.comboBoxLocation.Location = new System.Drawing.Point(89, 159);
+            this.comboBoxLocation.Name = "comboBoxLocation";
+            this.comboBoxLocation.Size = new System.Drawing.Size(175, 22);
+            this.comboBoxLocation.TabIndex = 9;
+            this.comboBoxLocation.SelectedIndexChanged += new System.EventHandler(this.comboBoxLocation_SelectedIndexChanged);
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(117, 205);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(147, 22);
+            this.textBoxPhone.TabIndex = 10;
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Enabled = false;
+            this.addressLabel.Location = new System.Drawing.Point(270, 74);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(112, 14);
+            this.addressLabel.TabIndex = 11;
+            this.addressLabel.Text = "Street Address:";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Enabled = false;
+            this.cityLabel.Location = new System.Drawing.Point(270, 116);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(42, 14);
+            this.cityLabel.TabIndex = 12;
+            this.cityLabel.Text = "City:";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Enabled = false;
+            this.stateLabel.Location = new System.Drawing.Point(270, 162);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(49, 14);
+            this.stateLabel.TabIndex = 13;
+            this.stateLabel.Text = "State:";
+            // 
+            // zipLabel
+            // 
+            this.zipLabel.AutoSize = true;
+            this.zipLabel.Enabled = false;
+            this.zipLabel.Location = new System.Drawing.Point(270, 210);
+            this.zipLabel.Name = "zipLabel";
+            this.zipLabel.Size = new System.Drawing.Size(70, 14);
+            this.zipLabel.TabIndex = 14;
+            this.zipLabel.Text = "ZIP Code:";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Enabled = false;
+            this.textBoxAddress.Location = new System.Drawing.Point(388, 71);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(135, 22);
+            this.textBoxAddress.TabIndex = 15;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Enabled = false;
+            this.textBoxCity.Location = new System.Drawing.Point(318, 113);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(205, 22);
+            this.textBoxCity.TabIndex = 16;
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.Enabled = false;
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Items.AddRange(new object[] {
+            "AL",
+            "AK",
+            "AZ",
+            "AR",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "FL",
+            "GA",
+            "HI",
+            "ID",
+            "IL",
+            "IN",
+            "IA",
+            "KS",
+            "KY",
+            "LA",
+            "ME",
+            "MD",
+            "MA",
+            "MI",
+            "MN",
+            "MS",
+            "MO",
+            "MT",
+            "NE",
+            "NV",
+            "NH",
+            "NJ",
+            "NM",
+            "NY",
+            "NC",
+            "ND",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VT",
+            "VA",
+            "WA",
+            "WV",
+            "WI",
+            "WY"});
+            this.comboBoxState.Location = new System.Drawing.Point(325, 159);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(198, 22);
+            this.comboBoxState.TabIndex = 17;
+            // 
+            // textBoxZip
+            // 
+            this.textBoxZip.Enabled = false;
+            this.textBoxZip.Location = new System.Drawing.Point(346, 205);
+            this.textBoxZip.Name = "textBoxZip";
+            this.textBoxZip.Size = new System.Drawing.Size(177, 22);
+            this.textBoxZip.TabIndex = 18;
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(12, 238);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 14);
+            this.labelError.TabIndex = 19;
+            // 
+            // linkLabelURL
+            // 
+            this.linkLabelURL.AutoSize = true;
+            this.linkLabelURL.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelURL.Location = new System.Drawing.Point(-4, 239);
+            this.linkLabelURL.Name = "linkLabelURL";
+            this.linkLabelURL.Size = new System.Drawing.Size(0, 13);
+            this.linkLabelURL.TabIndex = 20;
+            this.linkLabelURL.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelURL_LinkClicked);
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerate.Location = new System.Drawing.Point(202, 261);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(117, 35);
+            this.buttonGenerate.TabIndex = 21;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 308);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.linkLabelURL);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.textBoxZip);
+            this.Controls.Add(this.comboBoxState);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.zipLabel);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.comboBoxLocation);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.locationLabel);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.linkLabelKb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.h1label);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "IT Returns Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label h1label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabelKb;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.Label locationLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.ComboBox comboBoxLocation;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label zipLabel;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.ComboBox comboBoxState;
+        private System.Windows.Forms.TextBox textBoxZip;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.LinkLabel linkLabelURL;
+        private System.Windows.Forms.Button buttonGenerate;
+    }
+}
+
--- IT Returns Label Maker/IT Returns Label Maker/Program.cs
+++ IT Returns Label Maker/IT Returns Label Maker/Program.cs
@@ -1,23 +1,22 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ReturnLabelMaker
+namespace IT_Returns_Label_Maker
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ReturnsForm());
+            Application.Run(new Form1());
         }
     }
 }
